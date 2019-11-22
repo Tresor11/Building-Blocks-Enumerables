bb28d0fba5f5eba93a7662628c0b4c873e88275e
Tresor11 <tresor.moise2001@gmail.com>
2019-11-22 16:04:57 +0200

    created my collect

--- method.rb
+++ method.rb
@@ -1,12 +1,31 @@
 # frozen_string_literal: true
 
 module Enumerable
-  def my_each(arr)
+  def each(arr)
     result = []
     i = 0
     until i == arr.length
-      res << yield(arr[i])
+      result << yield(arr[i])
       i += 1
+    end
+  end
+
+  def each_with_index(arr)
+    result = []
+    i = 0
+    until i == arr.length
+      result << yield(arr[i])
+      i += 1
+    end
+  end
+
+  def select(arr)
+    result = []
+    i = 0
+    until i == arr.length
+      if yield(arr[i])
+        result << arr[i]
+      end
     end
   end
 end
